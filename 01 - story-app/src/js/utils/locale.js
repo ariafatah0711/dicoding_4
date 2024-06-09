6b46cc6e676ea8352e4c8dbe5c3e0d1a385cb500
Aria Fatah <ariafatah0711@gmail.com>
2024-06-09 14:37:10 +0700

    setup branch

--- 01 - story-app/src/js/utils/locale.js
+++
@@ -1,9 +0,0 @@
-import { setLocale, localeNames } from '../localization';
-
-const KEY = 'locale';
-const localeStorage = localStorage.getItem(KEY);
-
-// Jika ada preferensi bahasa yang tersimpan dalam localStorage, langsung atur bahasa
-if (localeStorage && localeNames.hasOwnProperty(localeStorage)) {
-  setLocale(localeStorage);
-}
--- 01 - story-app/src/js/utils/theme.js
+++
@@ -1,8 +0,0 @@
-const KEY = 'theme';
-const bodyElement = document.querySelector('body');
-if (!localStorage.getItem(KEY)) {
-  localStorage.setItem(KEY, 'light');
-}
-const theme = localStorage.getItem(KEY) ? localStorage.getItem(KEY) : 'light';
-
-bodyElement.setAttribute('data-bs-theme', theme);
